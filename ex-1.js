8b38e911d78c639883c06dd760f54aefced80f1f
chinawin <chinawin47@gmail.com>
2026-03-18 14:05:16 +0700

    reSubmit Assignment

--- ex-1.js
+++ ex-1.js
@@ -3,8 +3,8 @@
 Object.is(10, 20); //false
 Object.is(0, -0); //false
 Object.is(NaN, 100); //false
-Object.is("one", "two"); //true
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //true
+Object.is("one", "two"); //false
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false
 Object.is(null, undefined); //fasle
 
 10 === 20; //false
@@ -16,4 +16,4 @@ NaN === NaN; //false
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB; //true
+objectA === objectB; //false
